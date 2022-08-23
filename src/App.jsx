ed0ac6f1304380d369fdc1cf1793ed306eaf49be
Janisgee <100589152+Janisgee@users.noreply.github.com>
2022-08-23 19:27:25 +0800

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,18 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Change Calculator Application </h1> <Calculator />
+        <h1> Money Change Calculator</h1> <Calculator />
       </header>{" "}
+      <footer>
+        This project was coded by Janis Chan and is open-sourced on{" "}
+        <a
+          href="https://github.com/Janisgee/money-exchange-application"
+          title="_blank"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on Netlify.
+      </footer>
     </div>
   );
 }
